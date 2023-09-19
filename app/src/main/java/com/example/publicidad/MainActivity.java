c6951723d0b46a289f7369c44b66503663298e80
Jartonio <antonio@asturmail.com>
2023-09-19 10:20:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/publicidad/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.publicidad;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnOpenSecondActivity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
